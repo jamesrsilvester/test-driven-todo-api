a87b687b197891083ed60445e76820cc66ddc03e
jamesrsilvester <jamesrsilvester@gmail.com>
2017-07-17 10:36:20 -0700

    1 issue with delete remains

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 // require express and other modules
-var express = require('express'),
+let express = require('express'),
     app = express(),
     bodyParser = require('body-parser');
 
@@ -14,7 +14,7 @@ app.use(express.static(__dirname + '/public'));
  ************/
 
 // our database is an array for now with some hardcoded values
-var todos = [
+let todos = [
   { _id: 7, task: 'Laundry', description: 'Wash clothes' },
   { _id: 27, task: 'Grocery Shopping', description: 'Buy dinner for this week' },
   { _id: 44, task: 'Homework', description: 'Make this app super awesome!' }
@@ -54,13 +54,26 @@ app.get('/api/todos', function index(req, res) {
    */
    console.log('index get', todos);
    res.json({data:todos});
+
 });
 
 app.post('/api/todos', function create(req, res) {
   /* This endpoint will add a todo to our "database"
    * and respond with the newly created todo.
    */
-
+    //save form submission as new to do
+    let newToDo = req.body;
+    //print new task to terminal
+    console.log(newToDo);
+    //have newID =  ID of last task in array, then add 1.
+    let NewID = todos[todos.length-1]._id + 1;
+    //assign new id to the new task.
+    newToDo._id = NewID;
+    //add new todo item
+    todos.push(newToDo);
+    //print new list of todos with addition
+    console.log(todos);
+    res.json(newToDo);
 });
 
 app.get('/api/todos/:id', function show(req, res) {
@@ -81,6 +94,21 @@ app.put('/api/todos/:id', function update(req, res) {
    * id specified in the route parameter (:id) and respond
    * with the newly updated todo.
    */
+   let todoId = (req.params.id);
+
+    // find todo to update by its id
+    let todoUpdate = todos.filter(function (todo) {
+      return todo._id == todoId;
+    })[0];
+
+    // update the todo's task
+    todoUpdate.task = req.body.task;
+
+    // update the todo's description
+    todoUpdate.description = req.body.description;
+
+    res.json(todoUpdate);
+
 });
 
 app.delete('/api/todos/:id', function destroy(req, res) {
@@ -88,6 +116,14 @@ app.delete('/api/todos/:id', function destroy(req, res) {
    * id specified in the route parameter (:id) and respond
    * with success.
    */
+   //retrieve ID from URL
+  //  let todoforDelete = req.params.id;
+   for (let i=0; i<todos.length; i++) {
+     console.log("searching for match to delete..", i);
+     if (todos[i]._id==req.params.id) {todos.splice(todos[i], 1);
+       res.json(todos[i]);
+     }
+   }
 });
 
 /**********
